64c7c97451cd9a7ff528b20a5bca1515bfad46ea
subhash2807 <lcs2019016@iiitl.ac.in>
2020-04-13 19:30:25 +0530

    remove proxy

--- src/utils/coordinates.js
+++ src/utils/coordinates.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 
 const getCoordinates = (city,callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoicnVzdGFtNjciLCJhIjoiY2s4ajdwNXlzMGJmdTNocWc2eDZtMHRkeCJ9.nm-BhNdGV1ozXeRSgyutPw`;
-    request({url,json:true,proxy:'http://10.10.12.2:8080'},(err,res,body)=> {
+    request({url,json:true},(err,res,body)=> {
     if(err)
     {
         callback('error internet is not working',undefined,undefined);
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 var weather = (lat,lon,callback)=> {
     var url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e8cde246c8e175455f7354975fd34a4a`;
-    request({url,json:true,proxy:'http://10.10.12.2:8080'},(err,res,body)=> {
+    request({url,json:true},(err,res,body)=> {
         if(err)
         {
             // console.log(chal.red.bgWhite('connection not established with openweather'));
